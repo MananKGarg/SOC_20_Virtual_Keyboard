6bd1e752adb64ab7d29a4e3f3f14570aaf8a1636
Rushil Koppaka <62547838+rushilkoppaka@users.noreply.github.com>
2020-04-06 01:06:26 +0530

    Update Rushil_Hari_Koppaka_Question1.py
    didn't see we had to take input for no. of matches beforehand

--- Assignment 1/Team 6/Rushil_Hari_Koppaka_Question1.py
+++ Assignment 1/Team 6/Rushil_Hari_Koppaka_Question1.py
@@ -2,27 +2,27 @@ match_name_list=[]                                          #initialising lists
 match_list=[]
 final_player_names=[]
 final_scores=[]
-while True :                                                 #getting main dictionary(entire league)
-    match_name=input('enter match name,else: to finish type no \n')
-    if match_name=='no':
-        break
-    else:
-        match_name_list.append(match_name)
-        single_match_dict = {}
-        while True:                                           #getting inside dictionary(individual match)
-            check=input('to go next match type no else press enter\n')
-            if check=='no':
-                break
-            else:
-                player_name,runs_scored=input('player name\n'),int(input('runs scored\n'))
-                single_match_dict[player_name]=runs_scored
-                if player_name in final_player_names:                                             #for tuple scorecard incrementing score of individual
-                    final_scores[final_player_names.index(player_name)]+=runs_scored
-                else:
-                    final_player_names.append(player_name)
-                    final_scores.append(runs_scored)
 
-        match_list.append(single_match_dict)
+a=int(input('enter no.of matches:'))
+for i in range(0,a):                                                 #getting main dictionary(entire league)
+    match_name=input('enter match name:')
+    match_name_list.append(match_name)
+    single_match_dict = {}
+    while True:                                           #getting inside dictionary(individual match)
+        check=input('to go next match type no else press enter\n')
+        if check=='no':
+            print()
+            break
+        else:
+            player_name,runs_scored=input('player name:'),int(input('runs scored:'))
+            single_match_dict[player_name]=runs_scored
+            if player_name in final_player_names:                                             #for tuple scorecard incrementing score of individual
+                final_scores[final_player_names.index(player_name)]+=runs_scored
+            else:
+                final_player_names.append(player_name)
+                final_scores.append(runs_scored)
+
+    match_list.append(single_match_dict)
 
 final_dict=dict(zip(match_name_list,match_list))
 print(final_dict)
