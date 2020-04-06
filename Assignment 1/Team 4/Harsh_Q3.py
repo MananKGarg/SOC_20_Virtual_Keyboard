5c8219184c32c11114203809d791e08a1f68e725
gautam32 <62397517+gautam32@users.noreply.github.com>
2020-04-06 18:54:11 +0530

    Update Harsh_Q3.py

--- Assignment 1/Team 4/Harsh_Q3.py
+++ Assignment 1/Team 4/Harsh_Q3.py
@@ -4,31 +4,31 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 from scipy.cluster import vq
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser()  		#to read command line Arguments
 parser.add_argument('--input')
 parser.add_argument('--k', type=int)
 parser.add_argument('--output')
 args = parser.parse_args()
 
-img = mpimg.imread(args.input).astype(float)
+img = mpimg.imread(args.input).astype(float) 	# read the image as numpy array
 
-img = img[:, :, :3]
+img = img[:, :, :3]   				# trim the array , we need only RGB values
 
-t = img <= 1
+t = img <= 1 					#check whether the all the elements of array is < 1
 if not np.all(t):
-    img = img/255
+    img = img/255  				#if not divide by 255
 
 
-(r, c, ch) = img.shape
-img1 = img.reshape(r*c, ch)
+(r, c, ch) = img.shape   
+img1 = img.reshape(r*c, ch)			#reshape the array to 2D array
 
 try:
 	centroid, label = vq.kmeans2(img1, args.k, minit="++")
 except Exception as e:
-	centroid, label = vq.kmeans2(img1, args.k)
+	centroid, label = vq.kmeans2(img1, args.k)  #when minit="++" is not appropriate for the given inputs
 
 img1 = centroid[label]
 img1 = img1.reshape(r, c, 3)
-plt.imshow(img1)
-plt.savefig(args.output)
+plt.imshow(img1)  				# create image from the array
+plt.savefig(args.output)			#save the image at the desired place
 # plt.show()
