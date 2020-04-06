c1d6411f61d8d39c2e5f2effc574abeff01aca1e
Sudhansh6 <52414199+Sudhansh6@users.noreply.github.com>
2020-04-06 22:56:02 +0530

    Update Vishesh_Task4.py

--- Assignment 1/Team 3/Vishesh_Task4.py
+++ Assignment 1/Team 3/Vishesh_Task4.py
@@ -1,3 +1,19 @@
+import numpy as np
+
+def mean_filter(a,k):
+	
+	l = len(a)
+	b = np.zeros(l)
+	k1 = a[k-1:0:-1]+[a[0]]		#[a[0]]*a[0]
+	k2 = a[l:l-k-1:-1]		#[a[l-1]]*a[l-1]
+	a_ = np.array(k1+a+k2)
+	print(a_)
+	b = list(map(lambda i:np.average(a_[i-k:i+k+1]),range(k,k+l)))
+	print(b)
+
+
+# mean_filter([1,2,3,4,5,6,7,8,9],2)
+
 def generate_sin_wave(period, range_,num):
 	a=range_[0]
 	b=range_[1]
