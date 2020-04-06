a4fd65d7ef04b845bb98fbef764242d101e3cc53
Vishesh2k01 <62540995+Vishesh2k01@users.noreply.github.com>
2020-04-07 00:05:03 +0530

    Update Vishesh_Driver.py

--- Assignment 1/Team 3/Vishesh_Driver.py
+++ Assignment 1/Team 3/Vishesh_Driver.py
@@ -11,6 +11,20 @@ var=0.05
 x=[]
 clean_sin=[]
 dirty_sin=[]
+def mean_filter(a,k):
+	
+	l = len(a)
+	b = np.zeros(l) # Creates an array with all 0's of length of given array
+	k1 = a[k-1:0:-1]+[a[0]]	# Creates a reflection of elements of a for average at edges	#[a[0]]*a[0]
+	k2 = a[l:l-k-1:-1]	# Same as above	#[a[l-1]]*a[l-1]
+	a_ = np.array(k1+a+k2) # If a=[1,2,3,4] with k=2 then a_ = [2,1,1,2,3,4,4,3]
+	b = np.around(list(map(lambda i:np.average(a_[i-k:i+k+1]),range(k,k+l))),1) 
+	# Creates an array with each element as average of k elements around it
+	# for ex: a=[1,2,3,4] with k=2 returns [ 1.8  2.2  3.   4.   5.   6.   7.   7.8  8.2]
+	return b
+
+
+# mean_filter([1,2,3,4,5,6,7,8,9],2)
 
 def generate_sin_wave(period, range_,num):
 	a=range_[0]
@@ -28,14 +42,14 @@ def generate_sin_wave(period, range_,num):
 
 
 def noisify(array, var):
-	return np.random.normal(0,var,1000)
+	noise=np.random.normal(0,var,1000)
+	dirty_sin=clean_sin+noise
+	return dirty_sin
 
 	
 generate_sin_wave(period,range_,num)
 
 
-noise=noisify(clean_sin,var)
-dirty_sin=clean_sin+noise
 
 
 print(clean_sin)
@@ -54,3 +68,10 @@ plt.title("dirty_sin")
 plt.xlabel("X")  
 plt.ylabel("Y")  
 plt.show() 
+cleared_sin=mean_filter(dirty_sin,1)
+plt.plot(x,cleared_sin,color = 'red', marker = "o")  
+plt.title("cleared_sin")  
+plt.xlabel("X")  
+plt.ylabel("Y")  
+plt.show() 
+
