415bf9983ca977acbd003686d7e517b7d1d9228e
nirmalshah123 <62292293+nirmalshah123@users.noreply.github.com>
2020-04-06 15:04:20 +0530

    Update Q1.py

--- Assignment 1/Team 4/Q1.py
+++ Assignment 1/Team 4/Q1.py
@@ -28,7 +28,7 @@ for k, v in final_dict.items():
         else:
             common_dict[p] = v[p]
 
-print(common_dict)
+
 print(sorted(common_dict.items(), key =lambda kv:kv[0],reverse=True)) #as we have to do lexicographic order only if scores match up
 
 print(sorted(common_dict.items(), key =lambda kv:kv[1],reverse=True)) #This has to be done at the end as it is our main priority
