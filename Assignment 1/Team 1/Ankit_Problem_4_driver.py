dfd182dc1b994e7f2049b467185e437740b687d3
akj0811 <55907159+akj0811@users.noreply.github.com>
2020-04-06 21:27:31 +0530

    Update Ankit_Problem_4_driver.py

--- Assignment 1/Team 1/Ankit_Problem_4_driver.py
+++ Assignment 1/Team 1/Ankit_Problem_4_driver.py
@@ -1,12 +1,29 @@
 import matplotlib.pyplot as plt 
 import numpy as np
-from trial import generate_sine_wave, noisify, mean_filter
+from task4 import generate_sine_wave, noisify, mean_filter
   
-x = np.arange(-2, 8, 0.01)
+x_data = np.arange(-2, 8, 0.01)
+
 clean_sin = generate_sine_wave(2, [-2,8], 1000)
-dirty_sin = noisify(clean_sin, 0.05)
+plt.plot(x_data, clean_sin)
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+plt.title('Sine Function')
+plt.savefig('clean_sin.png')
+plt.show()
+
+dirty_sin = noisify(clean_sin, 0.05**2)
+plt.plot(x_data, dirty_sin)
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+plt.title('Noisy Sine Function!')
+plt.savefig('dirty_sin.png')
+plt.show()
+
 cleaned_sin = mean_filter(dirty_sin, 1)
-plt.plot(x, dirty_sin) 
-plt.plot(x, cleaned_sin)
-plt.plot(x, clean_sin)
-plt.show() 
+plt.plot(x_data, cleaned_sin)
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+plt.title('Filtered Sine Function')
+plt.savefig('cleaned_sin.png')
+plt.show()
