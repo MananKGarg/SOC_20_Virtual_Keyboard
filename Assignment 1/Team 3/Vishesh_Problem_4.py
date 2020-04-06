ddced3fd8407d936fbc0bbd263ce0a2b1a4fe1b8
Sudhansh6 <52414199+Sudhansh6@users.noreply.github.com>
2020-04-06 23:02:23 +0530

    Update Vishesh_Problem_4.py

--- Assignment 1/Team 3/Vishesh_Problem_4.py
+++ Assignment 1/Team 3/Vishesh_Problem_4.py
@@ -7,9 +7,8 @@ def mean_filter(a,k):
 	k1 = a[k-1:0:-1]+[a[0]]		#[a[0]]*a[0]
 	k2 = a[l:l-k-1:-1]		#[a[l-1]]*a[l-1]
 	a_ = np.array(k1+a+k2)
-	print(a_)
 	b = list(map(lambda i:np.average(a_[i-k:i+k+1]),range(k,k+l)))
-	print(b)
+	return b
 
 
 # mean_filter([1,2,3,4,5,6,7,8,9],2)
