1d3fd4896c13f7cb24fcf0f46c5ead666333beee
nirmalshah123 <62292293+nirmalshah123@users.noreply.github.com>
2020-04-06 15:02:15 +0530

    Update Q1.py

--- Assignment 1/Team 4/Q1.py
+++ Assignment 1/Team 4/Q1.py
@@ -1,6 +1,4 @@
-Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> n=int(input())   # Number of matches played
+ n=int(input())   # Number of matches played
 
 final_dict={}    
 match_list=dict()
